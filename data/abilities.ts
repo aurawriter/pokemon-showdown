4a2496e6c846abda24a2b50626ddf99a07f91682
aurawriter <30663874+aurawriter@users.noreply.github.com>
2023-06-25 22:50:48 -0700

    Add files via upload

--- data/abilities.ts
+++ data/abilities.ts
@@ -47,6 +47,32 @@ export const Abilities: {[abilityid: string]: AbilityData} = {
 		rating: 4,
 		num: 91,
 	},
+	dragonheart: {
+		onModifyTypePriority: -1,
+		onModifyType(move, pokemon) {
+			const noModifyType = [
+				'judgment', 'multiattack', 'naturalgift', 'revelationdance', 'technoblast', 'terrainpulse', 'weatherball',
+			];
+			if (move.type === 'Normal' && !noModifyType.includes(move.id) &&
+				!(move.isZ && move.category !== 'Status') && !(move.name === 'Tera Blast' && pokemon.terastallized)) {
+				move.type = 'Dragon';
+				move.typeChangerBoosted = this.effect;
+			}
+		},
+		onBasePowerPriority: 23,
+		onBasePower(basePower, pokemon, target, move) {
+			if (move.typeChangerBoosted === this.effect) return this.chainModify([4915, 4096]);
+		},
+		onDamagingHit(damage,target,source,move){
+		if(move.type === 'Dragon')
+			if($this.randomChance(10,10)){
+				target.trySetStatus('brn',target);
+			}
+		}
+		name: "Dragonheart",
+		rating: 4,
+		num: 182,
+	},
 	aerilate: {
 		onModifyTypePriority: -1,
 		onModifyType(move, pokemon) {
