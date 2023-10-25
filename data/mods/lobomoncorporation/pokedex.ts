ac5ce2c0e1aefb94cd2f4dbfccc2d00f4b4eeebf
aurawriter <30663874+aurawriter@users.noreply.github.com>
2023-10-25 16:09:32 -0700

    Add files via upload

--- data/mods/lobomoncorporation/pokedex.ts
+++ data/mods/lobomoncorporation/pokedex.ts
@@ -5,5 +5,17 @@ export const Pokedex: {[k: string]: ModdedSpeciesData} = {
 	types: ["Fire","Ghost"],
 	baseStats: {hp: 70, atk:120, def:80,spa:110,spd:80,spe:70},
 	abilities: {0:"Fourth Match Flame"},
-	}
+	},
+	stufful: {
+		num: 759,
+		name: "Stufful",
+		types: ["Fairy", "Fighting"],
+		baseStats: {hp: 70, atk: 75, def: 50, spa: 45, spd: 50, spe: 50},
+		abilities: {0: "Fluffy", 1: "Klutz", H: "Cute Charm"},
+		heightm: 0.5,
+		weightkg: 6.8,
+		color: "Pink",
+		evos: ["Bewear"],
+		eggGroups: ["Field"],
+	},
 };
