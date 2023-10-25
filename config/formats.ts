511b6de14c76d442b7f298ed707318ae04330240
aurawriter <30663874+aurawriter@users.noreply.github.com>
2023-10-25 10:44:17 -0700

    Add files via upload

--- config/formats.ts
+++ config/formats.ts
@@ -4364,4 +4364,12 @@ export const Formats: FormatList = [
 		battle: {trunc: Math.trunc},
 		ruleset: ['HP Percentage Mod', 'Cancel Mod', 'Desync Clause Mod', 'Max Team Size = 24', 'Max Move Count = 24', 'Max Level = 9999', 'Default Level = 100'],
 	},
+	{
+		name: "Lobomon Corporation",
+		
+		mod: 'lobomoncorporation',
+		searchShow: true,
+		debug: true,
+		banlist: ['Uber','AG','OU','UU','RU','NU','PU'],
+	},
 ];
