ede82ced407b09acedf45b7c33f93e5cab09ea23
aurawriter <30663874+aurawriter@users.noreply.github.com>
2023-10-27 11:44:47 -0700

    Update pokedex.ts

--- data/mods/lobomoncorporation/pokedex.ts
+++ data/mods/lobomoncorporation/pokedex.ts
@@ -1,9 +1,17 @@
 export const Pokedex: {[k: string]: ModdedSpeciesData} = {
 	scorchedgirl:{
-	num: -60102,
-	name: "Scorched Girl",
-	types: ["Fire","Ghost"],
-	baseStats: {hp: 70, atk:120, def:80,spa:110,spd:80,spe:70},
-	abilities: {0:"Fourth Match Flame"},
+		num: -60102,
+		name: "Scorched Girl",
+		types: ["Fire","Ghost"],
+		baseStats: {hp: 70, atk:120, def:80,spa:110,spd:80,spe:70},
+		abilities: {0:"Fourth Match Flame"},
 	},
+	onesinandhundredsofgooddeeds:{
+		num: -15030326,
+		name: "One Sin and Hundreds of Good Deeds",
+		types: ["Normal","Ghost"],
+		baseStats: {hp:100,atk:100,def:100,spa:100,spd:100,spe:100},
+		abilties: {0: "Penitence"},
+		
+	}
 };
