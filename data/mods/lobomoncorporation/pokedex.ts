9397b09087a25828d7432cc10e024e29f8db599c
aurawriter <30663874+aurawriter@users.noreply.github.com>
2023-10-26 18:02:45 -0700

    Update pokedex.ts

--- data/mods/lobomoncorporation/pokedex.ts
+++ data/mods/lobomoncorporation/pokedex.ts
@@ -6,16 +6,4 @@ export const Pokedex: {[k: string]: ModdedSpeciesData} = {
 	baseStats: {hp: 70, atk:120, def:80,spa:110,spd:80,spe:70},
 	abilities: {0:"Fourth Match Flame"},
 	},
-	stufful: {
-		num: 759,
-		name: "Stufful",
-		types: ["Fairy", "Fighting"],
-		baseStats: {hp: 70, atk: 75, def: 50, spa: 45, spd: 50, spe: 50},
-		abilities: {0: "Fluffy", 1: "Klutz", H: "Cute Charm"},
-		heightm: 0.5,
-		weightkg: 6.8,
-		color: "Pink",
-		evos: ["Bewear"],
-		eggGroups: ["Field"],
-	},
 };
