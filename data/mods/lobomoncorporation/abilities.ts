1e42b16173fb405067d58ab94331509eace0e383
aurawriter <30663874+aurawriter@users.noreply.github.com>
2023-10-27 12:17:36 -0700

    Update abilities.ts

--- data/mods/lobomoncorporation/abilities.ts
+++ data/mods/lobomoncorporation/abilities.ts
@@ -1,5 +1,5 @@
 export const Abilities: {[k: string]: ModdedAbilityData} = {
-	fourthmatchflame:{
+fourthmatchflame:{
 		onDamagingHit(damage, target, source, move) {
 			if (!target.hp) {
 				this.damage(source.baseMaxhp / 3, source, target, null, true);
@@ -8,8 +8,8 @@ export const Abilities: {[k: string]: ModdedAbilityData} = {
 		desc: "If this Pokemon is knocked out, that move's user loses 1/4 of its maximum HP, rounded down. If any active Pokemon has the Ability Damp, this effect is prevented.",
 		shortDesc: "If this Pokemon is KOed, that move's user loses 1/4 its max HP.",
 		name: "Fourth Match Flame",
-},
-	penitence:{
+	},
+penitence:{
 		onStart(pokemon) {
 			for (const ally of pokemon.adjacentAllies()) {
 				this.damage(target.baseMaxhp / 3, target, target);
