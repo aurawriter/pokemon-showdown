ba4f0c7d3855b41ebccb6050fff82b7353c7ed6e
aurawriter <30663874+aurawriter@users.noreply.github.com>
2023-10-27 16:27:01 -0700

    Update pokedex.ts

--- data/mods/lobomoncorporation/pokedex.ts
+++ data/mods/lobomoncorporation/pokedex.ts
@@ -6,7 +6,7 @@ export const Pokedex: {[k: string]: ModdedSpeciesData} = {
 		baseStats: {hp: 70, atk:120, def:80,spa:110,spd:80,spe:70},
 		abilities: {0:"Fourth Match Flame"},
 	},
-	onesinandhundredsofgooddeeds:{
+	onesin:{
 		num: -15030326,
 		name: "One Sin",
 		types: ["Normal","Ghost"],
