f27f1fa1f64e2fad109f5b6c0fb41083dfc8a14c
aurawriter <30663874+aurawriter@users.noreply.github.com>
2023-10-27 15:19:31 -0700

    Update pokedex.ts

--- data/mods/lobomoncorporation/pokedex.ts
+++ data/mods/lobomoncorporation/pokedex.ts
@@ -8,10 +8,16 @@ export const Pokedex: {[k: string]: ModdedSpeciesData} = {
 	},
 	onesinandhundredsofgooddeeds:{
 		num: -15030326,
-		name: "One Sin and Hundreds of Good Deeds",
+		name: "One Sin",
 		types: ["Normal","Ghost"],
 		baseStats: {hp:100,atk:100,def:100,spa:100,spd:100,spe:100},
 		abilities: {0: "Penitence"},
-		
+	},
+	thesnowqueen:{
+		num: -60137,
+		name: "The Snow Queen",
+		types: ["Ice"],
+		baseStats: {hp:100,atk:100,def:100,spa:100,spd:100,spe:100},
+		abilities: {0: "Frost Splinter"},
 	},
 };
