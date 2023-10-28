089655b6c590b5483242692f8d8281fa625f1756
aurawriter <30663874+aurawriter@users.noreply.github.com>
2023-10-28 13:49:08 -0700

    Update conditions.ts
    Implemented Despair hopefully

--- data/mods/lobomoncorporation/conditions.ts
+++ data/mods/lobomoncorporation/conditions.ts
@@ -1,5 +1,6 @@
 export const Conditions: {[id: string]: ModdedConditionData} = {
-fbt: {
+//Frostbite
+	fbt: {
 		name: 'fbt',
 		effectType: 'Status',
 		onStart(target, source, sourceEffect) {
@@ -17,4 +18,34 @@ fbt: {
 			this.damage(pokemon.baseMaxhp / 16);
 		},
 	},
+//Despair
+	dsp: {
+		name: 'dsp',
+		effectType: 'Status',
+		onStart(target, source, sourceEffect) {
+			if (sourceEffect && sourceEffect.effectType === 'Ability') {
+				this.add('-status', target, 'dsp', '[from] ability: ' + sourceEffect.name, '[of] ' + source);
+			} else {
+				this.add('-status', target, 'dsp');
+			}
+		},
+		
+		onModifySpD(spe, pokemon) {
+			// Paralysis occurs after all other Speed modifiers, so evaluate all modifiers up to this point first
+			spd = this.finalModify(spd);
+			spd = Math.floor(spd * 50 / 100);
+			return spd;
+		},
+		onModifyDef(def, pokemon) {
+			def = this.finalModify(def);
+			def = Math.floor(def * 50 / 100);
+			return def;
+		},
+		onBeforeMovePriority: 1,
+		onBeforeMove(pokemon) {
+			if (this.randomChance(1, 4)) {
+				this.add('cant', pokemon, 'dsp');
+				return false;
+			}
+		},
 };
