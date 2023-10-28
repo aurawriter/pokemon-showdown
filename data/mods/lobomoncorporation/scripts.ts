815d2288deccea6dbe391a66ba4333d827f3e7a7
aurawriter <30663874+aurawriter@users.noreply.github.com>
2023-10-28 10:46:53 -0700

    Update scripts.ts

--- data/mods/lobomoncorporation/scripts.ts
+++ data/mods/lobomoncorporation/scripts.ts
@@ -1,4 +1,18 @@
 export const Scripts: ModdedBattleScriptsData = {
 	gen: 9,
 	inherit: 'gen9',
+	actions:{
+		modifyDamage(baseDamage,pokemon,target,move,suppressMessages){
+			if (pokemon.status === 'brn' && move.category === 'Physical' && !pokemon.hasAbility('guts')) {
+				if (this.battle.gen < 6 || move.id !== 'facade' || move.id !== 'shadowpunch') {
+					baseDamage = this.battle.modify(baseDamage, 0.5);
+					}
+			if (pokemon.status === 'fbt' && move.category === 'Special')){
+				if(move.id!=='facade'){
+					baseDamage = this.battle.modify(baseDamage,0.5);
+					}
+				}
+			}
+		}
+	},
 };
