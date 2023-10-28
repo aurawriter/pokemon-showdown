eb213f3605ba6ccd4441d62210455973cf6b25d9
aurawriter <30663874+aurawriter@users.noreply.github.com>
2023-10-28 14:43:16 -0700

    Update pokedex.ts

--- data/mods/lobomoncorporation/pokedex.ts
+++ data/mods/lobomoncorporation/pokedex.ts
@@ -1,23 +1,30 @@
 export const Pokedex: {[k: string]: ModdedSpeciesData} = {
-	scorchedgirl:{
+	scorchedgirl: {
 		num: -60102,
 		name: "Scorched Girl",
 		types: ["Fire","Ghost"],
 		baseStats: {hp: 70, atk:120, def:80,spa:110,spd:80,spe:70},
 		abilities: {0:"Fourth Match Flame"},
 	},
-	onesin:{
+	onesin: {
 		num: -15030326,
 		name: "One Sin",
 		types: ["Normal","Ghost"],
 		baseStats: {hp:100,atk:100,def:100,spa:100,spd:100,spe:100},
 		abilities: {0: "Penitence"},
 	},
-	thesnowqueen:{
+	thesnowqueen: {
 		num: -60137,
 		name: "The Snow Queen",
 		types: ["Ice"],
 		baseStats: {hp:100,atk:100,def:100,spa:100,spd:100,spe:100},
 		abilities: {0: "Frost Splinter"},
+	},
+	standardtrainingdummyrabbit: {
+		num: 00000,
+		name: "Standard Training-Dummy Rabbit",
+		types: ["Electric","Normal"],
+		baseStats: {hp: 110,atk:55,def:90,spa:80,spd:90,spe:60},
+		abilities: {0: "Standard Training"},
 	},
 };
