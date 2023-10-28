c60c377de30042ee01b3c0e08fa3224fdbd5adad
aurawriter <30663874+aurawriter@users.noreply.github.com>
2023-10-28 10:58:02 -0700

    Create items.ts

---
+++ data/mods/lobomoncorporation/items.ts
@@ -0,0 +1,17 @@
+export const Items: {[k: string]: ModdedItemData} = {
+  frostorb: {
+		name: "Frost Orb",
+		spritenum: 145,
+		fling: {
+			basePower: 30,
+			status: 'fbt',
+		},
+		onResidualOrder: 28,
+		onResidualSubOrder: 3,
+		onResidual(pokemon) {
+			pokemon.trySetStatus('fbt', pokemon);
+		},
+		num: 273,
+		gen: 4,
+	},
+};
