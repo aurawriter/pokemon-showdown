018c2a037fbe152d6061529868db1817d096ceed
aurawriter <30663874+aurawriter@users.noreply.github.com>
2023-11-01 11:45:27 -0700

    Update pokedex.ts

--- data/mods/lobomoncorporation/pokedex.ts
+++ data/mods/lobomoncorporation/pokedex.ts
@@ -1,4 +1,11 @@
 export const Pokedex: {[k: string]: ModdedSpeciesData} = {
+	standardtrainingdummyrabbit: {
+		num: -1000,
+		name: "Standard Training-Dummy Rabbit",
+		types: ["Electric","Normal"],
+		baseStats: {hp: 110,atk:55,def:90,spa:80,spd:90,spe:60},
+		abilities: {0: "Standard Training"},
+	},
 	scorchedgirl: {
 		num: -60102,
 		name: "Scorched Girl",
@@ -13,25 +20,46 @@ export const Pokedex: {[k: string]: ModdedSpeciesData} = {
 		baseStats: {hp:100,atk:100,def:100,spa:100,spd:100,spe:100},
 		abilities: {0: "Penitence"},
 	},
+	queenofhatred: {
+		num: -150104,
+		name: "Queen of Hatred",
+		types: ["Fairy"],
+		baseStats: {hp:100,atk:100,def:100,spa:100,spd:100,spe:100},
+		abilities: {0: "Love and Hate"},
+	},
+	queenofhatredbreach:{
+		num: -150104,
+		name: "Queen of Hatred-Breach",
+		types: ["Fairy","Dragon"],
+		baseStats: {hp:100,atk:100,def:100,spa:100,spd:100,spe:100},
+		abilities: {0: "Love and Hate"},
+	},
+	happyteddybear: {
+		num: -200406,
+		name: "Happy Teddy Bear",
+		types: ["Normal","Fighting"],
+		baseStats: {hp: 100,atk: 100, def: 100, spa: 100, spd: 100, spe: 100},
+		abilities: {0: "Bear Paws"},
+	},
+	oldlady: {
+		num: -150112,
+		name: "Old Lady",
+		types: ["Ghost","Psychic"],
+		baseStats: {hp: 100, atk: 100, def: 100, spa: 100, spd: 100, spe: 100},
+		abilities: {0: "Piling Story"},
+	},
+	onepoint76MHz: {
+		num: -200627,
+		name: "1.76 MHz",
+		types: ["Electric"],
+		baseStats: {hp: 100, atk: 100, def: 100, spa: 100, spd: 100, spe: 100},
+		abilities: {0: "Noise"},
+	},
 	thesnowqueen: {
 		num: -60137,
 		name: "The Snow Queen",
 		types: ["Ice"],
 		baseStats: {hp:100,atk:100,def:100,spa:100,spd:100,spe:100},
 		abilities: {0: "Frost Splinter"},
-	},
-	standardtrainingdummyrabbit: {
-		num: -1000,
-		name: "Standard Training-Dummy Rabbit",
-		types: ["Electric","Normal"],
-		baseStats: {hp: 110,atk:55,def:90,spa:80,spd:90,spe:60},
-		abilities: {0: "Standard Training"},
-	},
-	thequeenofhatred: {
-		num: -150104,
-		name: "Queen of Hatred",
-		types: ["Fairy"],
-		baseStats: {hp:100,atk:100,def:100,spa:100,spd:100,spe:100},
-		abilities: {0: "Love and Hate"},
 	},
 };
