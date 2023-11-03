57246e7076a6ec7aeb2b73a1a18731aa81e44982
aurawriter <30663874+aurawriter@users.noreply.github.com>
2023-11-02 18:58:39 -0700

    Update items.ts

--- data/mods/lobomoncorporation/items.ts
+++ data/mods/lobomoncorporation/items.ts
@@ -14,4 +14,22 @@ export const Items: {[k: string]: ModdedItemData} = {
 		num: 273,
 	   desc: "At the end of every turn, this item attempts to frostbite the holder.",
 	},
+	redshoes: {
+		name: "Red Shoes",
+		spritenum: 249,
+		fling: {
+			basePower: 30,
+		},
+		onModifyDamage(damage, source, target, move) {
+			return this.chainModify([5324, 4096]);
+		},
+		onAfterMoveSecondarySelf(source, target, move) {
+			if (source && source !== target && move && move.category !== 'Status' && !source.forceSwitchFlag) {
+				this.damage(source.baseMaxhp / 10, source, source, this.dex.items.get('lifeorb'));
+			}
+		},
+		num: 270,
+		desc: "Holder's attacks do 1.3x damage, and it loses 1/10 its max HP after the attack.",
+		damage: "The shoes bit [POKEMON]!",
+	},
 };
