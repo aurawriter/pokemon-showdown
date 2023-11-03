011adcca7f4650849428e16752ec67af6f589a64
aurawriter <30663874+aurawriter@users.noreply.github.com>
2023-11-02 21:02:17 -0700

    Update abilities.ts

--- data/mods/lobomoncorporation/abilities.ts
+++ data/mods/lobomoncorporation/abilities.ts
@@ -195,4 +195,54 @@ penitence:{
 		name: "Full Bloom",
 		desc: "When Beauty and the Beast dies, its curse continues"
 	},
+	nightmareofchristmas: {
+		onDamagingHit(damage, target, source, move) {
+			this.field.setWeather('snow');
+		},
+		name: "Nightmare of Christmas",
+		rating: 1,
+		num: 245,
+	},
+	deadoralive: {
+		onStart(pokemon) {
+			pokemon.abilityState.choiceLock = "";
+		},
+		onBeforeMove(pokemon, target, move) {
+			if (move.isZOrMaxPowered || move.id === 'struggle') return;
+			if (pokemon.abilityState.choiceLock && pokemon.abilityState.choiceLock !== move.id) {
+				// Fails unless ability is being ignored (these events will not run), no PP lost.
+				this.addMove('move', pokemon, move.name);
+				this.attrLastMove('[still]');
+				this.debug("Disabled by Dead or Alive");
+				this.add('-fail', pokemon);
+				return false;
+			}
+		},
+		onModifyMove(move, pokemon) {
+			if (pokemon.abilityState.choiceLock || move.isZOrMaxPowered || move.id === 'struggle') return;
+			pokemon.abilityState.choiceLock = move.id;
+		},
+		onModifySpePriority: 1,
+		onModifySpe(spe, pokemon) {
+			if (pokemon.volatiles['dynamax']) return;
+			// PLACEHOLDER
+			this.debug('Dead or Alive speed boost');
+			return this.chainModify(1.5);
+		},
+		onDisableMove(pokemon) {
+			if (!pokemon.abilityState.choiceLock) return;
+			if (pokemon.volatiles['dynamax']) return;
+			for (const moveSlot of pokemon.moveSlots) {
+				if (moveSlot.id !== pokemon.abilityState.choiceLock) {
+					pokemon.disableMove(moveSlot.id, false, this.effectState.sourceEffect);
+				}
+			}
+		},
+		onEnd(pokemon) {
+			pokemon.abilityState.choiceLock = "";
+		},
+		name: "Dead or Alive",
+		rating: 4.5,
+		num: 255,
+	},
 };
