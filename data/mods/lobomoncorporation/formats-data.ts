b6d3cf14570e0aab5db8bd1a2f802cd4c210eb9b
aurawriter <30663874+aurawriter@users.noreply.github.com>
2023-11-04 21:56:35 -0700

    Update formats-data.ts

--- data/mods/lobomoncorporation/formats-data.ts
+++ data/mods/lobomoncorporation/formats-data.ts
@@ -1,11 +1,52 @@
 export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
+standardtrainingdummyrabbit:{
+	tier: "TETH",
+},
 scorchedgirl: {
-		tier: "OU",
-	},
+		tier: "TETH",
+},
 onesin: {
-		tier: "OU",	
-	},
+		tier: "ZAYIN",	
+},
+queenofhatred: {
+	tier: "WAW",
+},
+happyteddybear: {
+	tier: "HE",
+},
+oldlady: {
+	tier: "TETH",
+},
+"176mhz": {
+	tier: "TETH",
+},
+singingmachine: {
+	tier: "HE",
+},
+warmheartedwoodsman: {
+	tier: "HE",
+},
 thesnowqueen: {
-		tier: "OU",
-	},
-}
+		tier: "HE",
+},
+bigbird: {
+	tier: "WAW",
+},
+allaroundhelper: {
+	tier: "HE",
+},
+snowwhitesapple: {
+	tier: "WAW",
+},
+beautyandthebeast: {
+	tier: "TETH",
+},
+rudolta: {
+	tier: "HE",
+},
+queenbee: {
+	tier: "WAW",
+},
+bloodbath: {
+	tier: "TETH",
+},
