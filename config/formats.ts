299346ad5d4406802b3c9860f3185e7893f09c6e
aurawriter <30663874+aurawriter@users.noreply.github.com>
2023-11-04 21:39:48 -0700

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -4373,6 +4373,6 @@ export const Formats: FormatList = [
 		mod: 'lobomoncorporation',
 		searchShow: true,
 		debug: true,
-		ruleset: ['Standard NatDex'],
+		ruleset: ['Standard NatDex', 'Terastal Clause', 'OHKO Clause', 'Evasion Moves Clause', 'Species Clause', 'Dynamax Clause', 'Sleep Clause Mod', 'Z-Move Clause'],
 	},
 ];
