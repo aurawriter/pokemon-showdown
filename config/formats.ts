1a330126c51c01357a905674de73a2224a711aa4
aurawriter <30663874+aurawriter@users.noreply.github.com>
2023-11-21 00:33:21 -0800

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -4735,7 +4735,7 @@ export const Formats: FormatList = [
 
 		mod: 'auroraformat',
 		debug: true,
-		ruleset: ['Standard',"Terastal Clause"],
+		ruleset: ['Standard NatDex','Terastal Clause', 'OHKO Clause', 'Evasion Moves Clause', 'Species Clause', 'Dynamax Clause', 'Sleep Clause Mod', 'Z-Move Clause'],
 		banlist: ['Uber', 'AG', 'Arena Trap', 'Moody', 'Sand Veil', 'Shadow Tag', 'Snow Cloak', 'King\'s Rock', 'Razor Fang', 'Baton Pass', 'Last Respects', 'Shed Tail'],
 	},
 	
