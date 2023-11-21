da29d6310d9925aaf0126722e517e04d374e990a
aurawriter <30663874+aurawriter@users.noreply.github.com>
2023-11-20 19:40:16 -0800

    Testing Aurora Format
    Making Aurora Format

---
+++ data/mods/auroraformat/formats-data.ts
@@ -0,0 +1,29 @@
+export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
+	cleffa: {
+		tier: "Aurora LC",
+	},
+	clefairy: {
+		tier: "Aurora NFE",
+	},
+	clefable: {
+		tier: "Aurora OU",
+	}
+	ekans: {
+		tier: "Aurora LC",
+	},
+	arbok: {
+		tier: "Aurora OU",
+	}
+	vulpix: {
+		tier: "Aurora NFE",
+	}
+	ninetales: {
+		tier: "Aurora OU",
+	},
+	vulpixalola: {
+		tier: "Aurora NFE",
+	}
+	ninetalesalola: {
+		tier: "Aurora OU",
+	}
+};
