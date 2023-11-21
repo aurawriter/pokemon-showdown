1adb707f6ec31c32102bd046b689af475d3220f0
aurawriter <30663874+aurawriter@users.noreply.github.com>
2023-11-20 19:42:41 -0800

    Update formats-data.ts

--- data/mods/auroraformat/formats-data.ts
+++ data/mods/auroraformat/formats-data.ts
@@ -7,23 +7,23 @@ export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
 	},
 	clefable: {
 		tier: "Aurora OU",
-	}
+	},
 	ekans: {
 		tier: "Aurora LC",
 	},
 	arbok: {
 		tier: "Aurora OU",
-	}
+	},
 	vulpix: {
 		tier: "Aurora NFE",
-	}
+	},
 	ninetales: {
 		tier: "Aurora OU",
 	},
 	vulpixalola: {
 		tier: "Aurora NFE",
-	}
+	},
 	ninetalesalola: {
 		tier: "Aurora OU",
-	}
+	},
 };
