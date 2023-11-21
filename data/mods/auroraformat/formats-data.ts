def359f060877a661d32590dda7c3d091962bb84
aurawriter <30663874+aurawriter@users.noreply.github.com>
2023-11-20 19:55:42 -0800

    Update formats-data.ts

--- data/mods/auroraformat/formats-data.ts
+++ data/mods/auroraformat/formats-data.ts
@@ -1,29 +1,65 @@
 export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
+	bulbasaur: {
+		tier: "[Aurora] LC",
+	},
+	ivysaur: {
+		tier: "[Aurora] NFE"
+	},
+	venusaur: {
+		tier: "[Aurora] OU",
+	},
+	charmander: {
+		tier: "[Aurora] LC",
+	},
+	charmeleon: {
+		tier: "[Aurora] NFE",
+	},
+	charizard: {
+		tier: "[Aurora] OU",
+	},
+	squirtle: {
+		tier: "[Aurora] LC",
+	},
+	wartortle: {
+		tier: "[Aurora] NFE",
+	},
+	blastoise: {
+		tier: "[Aurora] OU",
+	},
+	caterpie: {
+		tier: "Illegal",
+	}
+	metapod: {
+		tier: "Illegal",
+	},
+	butterfree: {
+		tier: "Illegal",
+	},
 	cleffa: {
-		tier: "Aurora LC",
+		tier: "[Aurora] LC",
 	},
 	clefairy: {
-		tier: "Aurora NFE",
+		tier: "[Aurora] NFE",
 	},
 	clefable: {
-		tier: "Aurora OU",
+		tier: "[Aurora] OU",
 	},
 	ekans: {
-		tier: "Aurora LC",
+		tier: "[Aurora] LC",
 	},
 	arbok: {
-		tier: "Aurora OU",
+		tier: "[Aurora] OU",
 	},
 	vulpix: {
-		tier: "Aurora NFE",
+		tier: "[Aurora] NFE",
 	},
 	ninetales: {
-		tier: "Aurora OU",
+		tier: "[Aurora] OU",
 	},
 	vulpixalola: {
-		tier: "Aurora NFE",
+		tier: "[Aurora] NFE",
 	},
 	ninetalesalola: {
-		tier: "Aurora OU",
+		tier: "[Aurora] OU",
 	},
 };
