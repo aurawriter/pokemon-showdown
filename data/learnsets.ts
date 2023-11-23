cf87c0429111ec77d933f47d6f73715aa2072a1a
aurawriter <30663874+aurawriter@users.noreply.github.com>
2023-11-22 16:22:43 -0800

    Update learnsets.ts

--- data/learnsets.ts
+++ data/learnsets.ts
@@ -1,6 +1,6 @@
 /* eslint-disable max-len */
 
-export const Learnsets: {[speciesid: string]: LearnsetData} = {
+export const Learnsets: {[k: string]: LearnsetData} = {
 	missingno: {
 		learnset: {
 			blizzard: ["3L1"],
