ef7d4b051ba336e3045048e39acc950182ba4893
aurawriter <30663874+aurawriter@users.noreply.github.com>
2023-12-04 20:54:06 -0800

    Update abilities.ts

--- data/abilities.ts
+++ data/abilities.ts
@@ -5937,7 +5937,7 @@ export const Abilities: {[abilityid: string]: AbilityData} = {
 			if (target === source || move.category === 'Status' || move.type === '???' || move.id === 'struggle') return;
 			if (move.id === 'skydrop' && !source.volatiles['skydrop']) return;
 			this.debug('Silver Lining immunity: ' + move.id);
-			if (target.runEffectiveness(move) == 0) {
+			if (target.runEffectiveness(move) == 1) {
 				if (move.smartTarget) {
 					move.smartTarget = false;
 				} else {
