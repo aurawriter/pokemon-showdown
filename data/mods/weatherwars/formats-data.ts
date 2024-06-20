fcbf0014041c3b971f520e7e9808fdf390a23479
aurawriter <30663874+aurawriter@users.noreply.github.com>
2024-06-19 18:54:58 -0700

    test
    test

---
+++ data/mods/weatherwars/formats-data.ts
@@ -0,0 +1,71 @@
+export const FormatsData: {[k: string]: SpeciesFormatsData} = {
+	sneevil: {
+		tier: "WeatherWars",
+	},
+	sneevilshaved: {
+		tier: "WeatherWars",
+	},
+	grizzweet: {
+		tier: "WeatherWars",
+	},
+	ponchobake: {
+		tier: "WeatherWars",
+	},
+	revolture: {
+		tier: "WeatherWars",
+	},
+	lemurei: {
+		tier: "WeatherWars",
+	},
+	faengpire: {
+		tier: "WeatherWars",
+	},
+	glucoana: {
+		tier: "WeatherWars",
+	},
+	dratosphere: {
+		tier: "WeatherWars",
+	},
+	citrillon: {
+		tier: "WeatherWars",
+	},
+	mothiosa: {
+		tier: "WeatherWars",
+	},
+	onisen: {
+		tier: "WeatherWars",
+	},
+	scabbardaunt: {
+		tier: "WeatherWars",
+	},
+	cobrust: {
+		tier: "WeatherWars",
+	},
+	lollibop: {
+		tier: "WeatherWars",
+	},
+	hypnoxious: {
+		tier: "WeatherWars",
+	},
+	volkaison: {
+		tier: "WeatherWars",
+	},
+	spiritombaurora: {
+		tier: "WeatherWars",
+	},
+	galacastarid: {
+		tier: "WeatherWars",
+	},
+	galacasthumid: {
+		tier: "WeatherWars",
+	},
+	santai: {
+		tier: "WeatherWars",
+	},
+	peeltite: {
+		tier: "WeatherWars",
+	},
+	citrabell: {
+		tier: "WeatherWars",
+	},
+};
