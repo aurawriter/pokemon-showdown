7249313fef306fc5d6f692ed584359fd863e580b
aurawriter <30663874+aurawriter@users.noreply.github.com>
2024-12-23 18:33:29 -0800

    Update moves.ts

--- data/moves.ts
+++ data/moves.ts
@@ -23737,7 +23737,7 @@ export const Moves: {[moveid: string]: MoveData} = {
 		accuracy: true,
 		basePower: 0,
 		category: "Status",
-		name: "Stellar Wish",
+		name: "Stellar Wish 1",
 		pp: 10,
 		priority: 0,
 		flags: {snatch: 1, heal: 1},
@@ -23749,6 +23749,58 @@ export const Moves: {[moveid: string]: MoveData} = {
 			},
 			onResidualOrder: 4,
 			onEnd(target) {
+				if (target && !target.fainted) {
+					if (this.effectState.bestStat == 'atk')
+					{
+						this.boost({atk: 2}
+					}
+					if (this.effectState.bestStat == 'def')
+					{
+						this.boost({def: 2}
+					}
+					if (this.effectState.bestStat == 'spa')
+					{
+						this.boost({spa: 2}
+					}
+					if (this.effectState.bestStat == 'spd')
+					{
+						this.boost({spd: 2}
+					}
+					if (this.effectState.bestStat == 'spe')
+					{
+						this.boost({spe: 2}
+					}
+					//const damage = this.heal(this.effectState.hp, target, target);
+					//if (damage) {
+						//this.add('-heal', target, target.getHealth, '[from] move: Stellar Wish', '[wisher] ' + this.effectState.source.name);
+					//}
+				}
+			},
+		},
+		secondary: null,
+		target: "self",
+		type: "Cosmic",
+		zMove: {boost: {spd: 1}},
+		contestType: "Cute",
+	},
+	stellarwish2: {
+		num: 273,
+		accuracy: true,
+		basePower: 0,
+		category: "Status",
+		name: "Stellar Wish 2",
+		pp: 10,
+		priority: 0,
+		flags: {snatch: 1, heal: 1},
+		slotCondition: 'Stellar Wish',
+		condition: {
+			duration: 2,
+			//onStart(pokemon, source) {
+			
+			//},
+			onResidualOrder: 4,
+			onEnd(target) {
+				this.effectState.bestStat = target.getBestStat(false, true);
 				if (target && !target.fainted) {
 					if (this.effectState.bestStat == 'atk')
 					{
