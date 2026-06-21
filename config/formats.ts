4c2085fe760a1079a41f2b635a8dc3f1c5e269ff
aurawriter <30663874+aurawriter@users.noreply.github.com>
2026-06-21 11:36:29 -0700

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -73,7 +73,7 @@ export const Formats: FormatList = [
 		gameType: 'doubles',
 		searchShow: false,
 		bestOfDefault: true,
-		ruleset: ['Standard NatDex','Terastal Clause', 'Item Clause', 'Adjust Level = 50', 'Min Source Gen = 9', 'VGC Timer', 'Open Team Sheets'],
+		ruleset: ['Standard NatDex','Terastal Clause', 'Item Clause', 'Adjust Level = 50', 'Min Source Gen = 9', 'VGC Timer', 'Open Team Sheets','Best of = 3'],
 	},
 	{
 		name: "[Gen 9 Aurora] Aurora Almost Any Ability",
